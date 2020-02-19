f1afc473ec40297bc5d5ecade9af11f4604f3f89
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-02-19 15:48:44 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var ed25519_sigs_js_1 = require("ed25519-sigs-js");
-exports.gen_pubKey = ed25519_sigs_js_1.gen_pubKey;
-exports.gen_privKey = ed25519_sigs_js_1.gen_privKey;
+exports.gen_keypair = ed25519_sigs_js_1.gen_keypair;
+exports.pubKey_from_pair_bytes = ed25519_sigs_js_1.pubKey_from_pair_bytes;
 exports.sign = ed25519_sigs_js_1.sign;
 exports.verify = ed25519_sigs_js_1.verify;
 exports.seed_from_phrase = ed25519_sigs_js_1.seed_from_phrase;
