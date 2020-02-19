65c458d63b5e2c188a81dac31e774fa5b3302554
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-02-19 18:41:02 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var ed25519_sigs_js_1 = require("ed25519-sigs-js");
 exports.random_new_keypair = ed25519_sigs_js_1.gen_keypair;
-exports.new_pubKey = ed25519_sigs_js_1.pubKey_from_pair_bytes;
+exports.new_pubkey = ed25519_sigs_js_1.pubKey_from_pair_bytes;
 exports.signature = ed25519_sigs_js_1.sign;
 exports.verification = ed25519_sigs_js_1.verify;
 exports.mnemonic_phrase_to_entropy = ed25519_sigs_js_1.seed_from_phrase;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {
   gen_keypair as random_new_keypair,
-  pubKey_from_pair_bytes as new_pubKey,
+  pubKey_from_pair_bytes as new_pubkey,
   sign as signature,
   verify as verification,
   seed_from_phrase as mnemonic_phrase_to_entropy
